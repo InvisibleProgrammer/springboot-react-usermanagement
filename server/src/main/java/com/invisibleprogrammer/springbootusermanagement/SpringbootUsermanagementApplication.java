0b947a0c78deeb1a1734f700d196c9080615c3da
Zsolt Miskolczi <zsolt.miskolczi@gmail.com>
2021-01-08 16:02:24 +0100

    Skeleton for the server

---
+++ server/src/main/java/com/invisibleprogrammer/springbootusermanagement/SpringbootUsermanagementApplication.java
@@ -0,0 +1,13 @@
+package com.invisibleprogrammer.springbootusermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUsermanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootUsermanagementApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/invisibleprogrammer/springbootusermanagement/SpringbootUsermanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invisibleprogrammer.springbootusermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUsermanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
